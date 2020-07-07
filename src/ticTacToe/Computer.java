27c87fbd8df573bbba3b0ebf176cbbf94ccc49d9
Kaleb Lyda <kalebal@live.unc.edu>
2020-07-06 21:14:04 -0400

    fixed infinite loop when computer's turn lands on a draw

--- src/ticTacToe/Computer.java
+++ src/ticTacToe/Computer.java
@@ -2,27 +2,34 @@ package ticTacToe;
 import java.util.Random;
 
 public class Computer {
-	Random random = new Random();
-	Field field;
+    Random random = new Random();
+    Field field;
 
-	public Computer(Field field) {
-		this.field = field;
-	}
-	
-	//picks a cell on the board at random
-	//if cell is occupied, continues to pick until an empty cell is found
-	//once an empty cell is found, sets that cell to "O"
-	public void makeEasyMove() {
-		System.out.println("Making move level \"easy\"");
-		
-		int x = random.nextInt(3);
-		int y = random.nextInt(3);
-		
-		while(!field.getCell(x, y).equals("_")) {
-			x = random.nextInt(3);
-			y = random.nextInt(3);
-		}
-		field.setCell(x, y, "O");
-	}
+    public Computer(Field field) {
+        this.field = field;
+    }
+
+    //picks a cell on the board at random
+    //if cell is occupied, continues to pick until an empty cell is found
+    //once an empty cell is found, sets that cell to "O"
+    public void makeEasyMove() {
+        System.out.println("Making move level \"easy\"");
+        field.tallyCells();
+        if(field.getOs() + field.getXs() != 9){
+            int x = random.nextInt(3);
+            int y = random.nextInt(3);
+
+            while(!field.getCell(x, y).equals("_")) {
+                x = random.nextInt(3);
+                y = random.nextInt(3);
+            }
+            field.setCell(x, y, "O");
+        } else {
+            Main.setGameState();
+        }
+
+
+    }
 
 }
+
